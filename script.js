2de4a3ec9150793b159a146320170c6db16ee17a
Bleedem <jjacobwow@gmail.com>
2023-10-23 04:24:23 -0400

    weather app lol

--- script.js
+++ script.js
@@ -16,21 +16,30 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name, icon, description, temp, humidity, speed)
-        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innterText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1920x1080/?" + name + "')";    
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
-    }
+      },
 };
 
-document
-.querySelector(".search button")
-.addEventListener("click", function() {
+document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
+  });
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
 })
+
+weather.fetchWeather("toronto");
